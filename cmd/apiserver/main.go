2002ee65467ec00f39809674f7b81dcff2d2782f
Danila Belov <newliferedbird@gmail.com>
2020-07-13 19:24:47 +0300

    1st stage

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,3 +1,32 @@
- package main
+package main
 
- import "github.com/newliferedbird/http_rest_api/app/apiserver"
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/newliferedbird/http_rest_api/internal/app/apiserver"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "config/apiserver.toml", "path to config file")
+}
+
+func main() {
+
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
